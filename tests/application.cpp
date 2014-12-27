b7a248626b0fd67196da59cea118fc04e8098edb
Kacper Kołodziej <kacper@kolodziej.in>
2014-12-27 18:59:01 +0100

    fixed test

--- tests/application.cpp
+++ tests/application.cpp
@@ -1,17 +1,23 @@
-#include "gtest/gtest.hpp"
+#include "gtest/gtest.h"
 
 #include "application.hpp"
+
+#include <cstring>
 
 using ncursesplus::Application;
 
 class TestApp : public Application
 {
 public:
+	TestApp(int argc, char** argv) :
+		Application(argc, argv)
+	{}
 	bool checkArgv(int argc, char** argv)
 	{
+		auto argv_ = getArgv_();
 		for (int i = 0; i < argc; ++i)
 		{
-			if (argv_[i] != argv[i])
+			if (strcmp(argv_[i].data(), argv[i]) != 0)
 			{
 				return false;
 			}
@@ -27,13 +33,15 @@ public:
 TEST(ApplicationTest, main)
 {
 	int ac = 5;
-	char** av = {
-		"one",
-		"two",
-		"three",
-		"four",
-		"five"
-	};
-	Application app(ac, av);
+	char** av = new char*[ac];
+	std::stringstream str;
+	for (int i = 0; i < ac; ++i)
+	{
+		char* buf = new char[80];
+		sprintf(buf, "param %03d", i);
+		av[i] = buf;
+	}
+	TestApp app(ac, av);
 	ASSERT_TRUE(app.checkArgv(ac, av));
+	ASSERT_EQ(app.main(), 0);
 }
